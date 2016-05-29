34581b4be37d4e79495d57b8d56234014faf5c49
chansuke <moonset20@gmail.com>
2016-05-30 03:08:34 +0900

    Update package.json

--- config/dev.exs
+++ config/dev.exs
@@ -10,9 +10,11 @@ config :phoenix_board, PhoenixBoard.Endpoint,
   http: [port: 4000],
   debug_errors: true,
   code_reloader: true,
+  cache_static_lookup: false,
   check_origin: false,
-  watchers: []
-
+  watchers: [
+    node: ["node_modules/webpack/bin/webpack.js", "--watch", "--color"]
+  ]
 # Watch static and templates for browser reloading.
 config :phoenix_board, PhoenixBoard.Endpoint,
   live_reload: [
