18e9ccfb6cf35d50aa4ae7d4e3eb83b26593047d
chansuke <moonset20@gmail.com>
2016-05-31 00:37:19 +0900

    WIP setup

--- web/router.ex
+++ web/router.ex
@@ -9,18 +9,9 @@ defmodule PhoenixBoard.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", PhoenixBoard do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", PhoenixBoard do
-  #   pipe_through :api
-  # end
 end
---
+++ web/static/js/application.js
@@ -0,0 +1,10 @@
+import React                  from 'react';
+import ReactDOM               from 'react-dom';
+import { browserHistory }     from 'react-router';
+import configureStore         from './store';
+import Root                   from './containers/root';
+
+const store = configureStore(browserHistory);
+const node = <Root routerHistory={browserHistory} store={store}/>;
+
+ReactDOM.render(node, target);
